42de3304abb20f4c27587324840d8005705a2690
kazuki-roshi <ganglinyigan@gmail.com>
2025-02-15 21:01:34 +0900

    課題再提出の為。

--- src/baseball/Review03.java
+++ src/baseball/Review03.java
@@ -11,21 +11,14 @@ public class Review03 {
         BaseBallTeam Carp = new BaseBallTeam("広島東洋カープ",66,74,3);
         BaseBallTeam Dragos = new BaseBallTeam("中日ドラゴンズ",66,75,2);
 
-        // 勝率を取得
-        double getRate1 = Swallows.getRate();
-        double getRate2 = Baystars.getRate();
-        double getRate3 = Tigers.getRate();
-        double getRate4 = Giants.getRate();
-        double getRate5 = Carp.getRate();
-        double getRate6 = Dragos.getRate();
 
         // reportメソッドを呼び出す
-        Swallows.report(getRate1);
-        Baystars.report(getRate2);
-        Tigers.report(getRate3);
-        Giants.report(getRate4);
-        Carp.report(getRate5);
-        Dragos.report(getRate6);
+        Swallows.report();
+        Baystars.report();
+        Tigers.report();
+        Giants.report();
+        Carp.report();
+        Dragos.report();
     }
 
 }
